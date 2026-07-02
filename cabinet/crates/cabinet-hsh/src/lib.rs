dc7ea18dc828539ace4a9464bbf3ade0da13094a
Sauomore <1928258545@qq.com>
2026-07-02 15:03:13 +0800

    feat: implement HSH-32 hierarchical semantic hashing with 32-bit continuous direction encoding

--- cabinet/crates/cabinet-hsh/src/lib.rs
+++ cabinet/crates/cabinet-hsh/src/lib.rs
@@ -10,9 +10,12 @@ pub mod hsh_code;
 pub mod perfect_hash;
 pub mod pos_map;
 
+pub mod hsh32;
+
 pub use cluster::{ClusterCenter, ClusterGroup, ClusterCenters, mock_embed};
 pub use encoder::{Encoder, EncoderConfig};
 pub use error::EncodeError;
 pub use hsh_code::HSHCode;
+pub use hsh32::{HSHCode32, Encoder32, PcaProjection, sign_quantize, is_antonym, is_antonym_default, direction_score, position_score, frequency_score, compute_score, compute_score_default, query_hsh32, query_hsh32_default, CandidateDoc, QueryResult, hamming_distance, explained_variance_ratio};
 pub use perfect_hash::{SeedTable, bkdr_hash, compute_abs, search_seed};
 pub use pos_map::{feat_name, pos_to_feat, FeatureCode};
